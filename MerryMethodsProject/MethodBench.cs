14c55cf6d5d4a22ff14882fac43fc1d74a1a79f2
NoO1ee <dominik0119@hotmail.com>
2024-09-19 15:02:36 +0200

    Add project files.

---
+++ MerryMethodsProject/MethodBench.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerryMethodsProject;
+
+internal class MethodBench
+{
+    public void FistMethod()
+    {
+        Console.WriteLine("Merry Methods");
+    }
+
+    public void SecondMethod(string args)
+    {
+        Console.WriteLine(args);
+    }
+
+    public void ThirdMethod(string input, bool isItTrue)
+    {
+        //if (isItTrue)
+        //    //SecondMethod(input.ToUpper());
+        //    input = input.ToUpper();
+        //else
+        //    //SecondMethod(input.ToLower());
+        //    input = input.ToLower();
+
+        // Ternary-operator ( ? = kolla) ( : = else)
+        input = isItTrue ? input.ToUpper() : input.ToLower();
+
+        SecondMethod(input);
+    }
+
+    public bool FourthMethod()
+    {
+        Console.Write("\nWould you like to [W]hisper or [S]hout?: \n");
+        var keypressed = Console.ReadKey(true).Key;
+
+        if (keypressed == ConsoleKey.S)
+            return true;
+        else if (keypressed == ConsoleKey.W)
+            return false;
+        else
+            return FourthMethod();
+            
+
+    }
+
+    public (string? Name, bool LengthyName) SixthMethod()
+    {
+        Console.Write("\nMata in ett namn: ");
+        string? Name = Console.ReadLine()!;
+        bool LengthyName = Name.Length > 5;
+
+
+        return (Name, LengthyName);
+
+    }
+    
+}
---
+++ MerryMethodsProject/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace MerryMethodsProject;
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        MethodBench method = new MethodBench();
+        int i = 0;
+        do
+        {
+            method.FistMethod();
+            i++;
+
+        } while (i < 3);
+        method.SecondMethod("C# Now and Forever!\n");
+        Console.Write("Enter something: ");
+        string? input = Console.ReadLine();
+        method.SecondMethod(input!);
+
+
+
+
+
+
+        
+
+        method.ThirdMethod(input!, Reply());
+        
+
+
+
+
+        Console.Write("\nType in a question. [S] is Upper case and [W] is lower case: ");
+        string? question = Console.ReadLine();
+        Console.WriteLine(question);
+        
+        string? check2 = Console.ReadLine()!.ToLower();
+
+        if (check2 == "w")
+            method.ThirdMethod(input!, false);
+        else if (check2 == "s")
+            method.ThirdMethod(input!, true);
+
+
+        bool shout = method.FourthMethod();
+        method.ThirdMethod(input!, shout);
+        method.ThirdMethod(input!, method.FourthMethod());
+
+        var sixth = method.SixthMethod();
+
+        if (sixth.LengthyName)
+            Console.WriteLine($"{sixth.Name} is a long name");
+        else
+            Console.WriteLine($"{sixth.Name} is not a long name");
+
+
+    }
+
+    static bool Reply()
+    {
+        Console.Write("\n[U] to print in upper case, otherwise lower case: ");
+        string? check = Console.ReadLine()!.ToLower();
+        return check == "u";
+    }
+}
